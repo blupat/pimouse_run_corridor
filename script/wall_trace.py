92951cf84e659bea4d38e0233a0e4a8694227fb8
Yasushi Oshima <oosmyss@gmail.com>
2019-06-29 05:21:49 +0000

    Use parameter server for servo settings

--- script/wall_trace.py
+++ script/wall_trace.py
@@ -8,6 +8,12 @@ class WallTrace():
     def __init__(self):
         self.cmd_vel = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
         
+        self.accel = rospy.get_param("/run_corridor/acceleration")
+        self.max_speed = rospy.get_param("/run_corridor/max_speed")
+        self.min_speed = rospy.get_param("/run_corridor/min_speed")
+        self.servo_target = rospy.get_param("/run_corridor/servo_target")
+        self.servo_kp = rospy.get_param("/run_corridor/servo_kp")
+        
         self.sensor_values = LightSensorValues()
         rospy.Subscriber('/lightsensors', LightSensorValues, self.callback)
 
@@ -18,27 +24,25 @@ class WallTrace():
         rate = rospy.Rate(20)
         data = Twist()
         
-        accel = 0.02
         data.linear.x = 0.0
         while not rospy.is_shutdown():
             s = self.sensor_values
-            data.linear.x += accel
+            data.linear.x += self.accel
             
             if s.sum_forward >= 50:
                 data.linear.x = 0.0
-            elif data.linear.x <= 0.2:
-                data.linear.x = 0.2
-            elif data.linear.x >= 0.8:
-                data.linear.x = 0.8
+            elif data.linear.x <= self.min_speed:
+                data.linear.x = self.min_speed
+            elif data.linear.x >= self.max_speed:
+                data.linear.x = self.max_speed
             
-            if data.linear.x < 0.2:
+            if data.linear.x < self.min_speed:
                 data.angular.z = 0.0
             elif s.left_side < 10:
                 data.angular.z = 0.0
             else:
-                target = 50
-                error = (target - s.left_side) / 50.0
-                data.angular.z = error * 3 * math.pi / 180.0
+                error = self.servo_target - s.left_side
+                data.angular.z = error * self.servo_kp * math.pi / 180.0
             
             self.cmd_vel.publish(data)
             rate.sleep()
